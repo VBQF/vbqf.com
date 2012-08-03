6e4f1c3201ab3f9e3c7d26b86b0b25d02f085d32
VBQF <info@vbqf.com>
2012-08-03 19:31:49 +0200

    Install of JA-Elastica Template

---
+++ logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2012-08-03 17:28:02 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2012-08-03	17:28:02	INFO	192.168.0.51	Joomla FAILURE: 	Le mot de passe ne correspond pas au nom d'utilisateur, ou vous n'avez pas encore de compte.
+2012-08-03	17:28:07	INFO	192.168.0.51	Joomla FAILURE: 	Le mot de passe ne correspond pas au nom d'utilisateur, ou vous n'avez pas encore de compte.
